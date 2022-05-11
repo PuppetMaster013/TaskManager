5fa20b2be367a52ad1f4ea31384fcfe516a98afa
aditya <adityakumar222333@gmail.com>
2022-05-12 01:08:07 +0530

    completed the numeric timer

--- src/Components/Timer/NumericTimer.js
+++ src/Components/Timer/NumericTimer.js
@@ -1,11 +1,14 @@
-import React, { useState,useEffect } from "react";
+import React, { useState,useEffect, useTransition } from "react";
 
 const NumericTimer = () => {
   const [minutes, setMinutes] = useState(25);
   const [seconds, setSeconds] = useState(0);
-
+  const [workingPeriod,setWorkingPeriod] = useState(true)
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+
+
 
   useEffect(() => {
       let interval = setInterval(() => {
@@ -16,10 +19,17 @@ const NumericTimer = () => {
                     setMinutes(minutes-1);
               }
               else{
-                  
+                //   this is time when the timer ends
+                  let minutes = workingPeriod ? 4:24;
+                  let seconds = 59
+                  setSeconds(seconds)
+                  setMinutes(minutes)
+                  setWorkingPeriod(!workingPeriod);
+
               }
           }
           else{
+            //   this happens every time the second is not zero
               setSeconds(seconds-1)
           }
       }, 1000);
